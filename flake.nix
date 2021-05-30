620161ebea0bab849488fc826253354e7d360e92
Justin Restivo <justin.p.restivo@gmail.com>
2021-05-30 16:38:26 -0400

    filling in the blanks

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Example for presentation";
   inputs = {
-    nixpkgs.url = "nixpkgs/master"; 
+    nixpkgs.url = "nixpkgs/master";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,47 +9,62 @@
 
     naersk = {
       url = "github:nmattia/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";  
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = inputs@{self, nixpkgs, rust-overlay, naersk, ... }:
-  let 
+  let
     pkgs = import nixpkgs {
       localSystem = "${system}";
-      overlays = [ rust-overlay.overlay ];
+      overlays = [
+        rust-overlay.overlay
+        (final: prev:
+        {
+          qemu = prev.qemu.overrideAttrs (oldAttrs: rec {
+            src = builtins.fetchurl {
+              url= "https://download.qemu.org/qemu-6.0.0.tar.xz";
+              sha256 = "1f9hz8rf12jm8baa7kda34yl4hyl0xh0c4ap03krfjx23i3img47";
+            };
+          });
+        }
+        )
+      ];
     };
     system = "x86_64-linux";
     riscvPkgs = import nixpkgs {
       localSystem = "${system}";
       crossSystem = {
         config = "riscv64-unknown-linux-gnu";
-        abi = "lp64d";
+        abi = "lp64";
       };
     };
     rust_build =
       pkgs.rust-bin.nightly."2021-05-10".default.override {
-        targets = [ "riscv64gc-unknown-none-elf" ];
+        targets = [ "riscv64imac-unknown-none-elf" ];
         extensions = [ "rust-src" "clippy" "cargo" "rustfmt-preview" ];
       };
-    naersk_lib = naersk.lib."${system}".override {
-      rustc = rust_build;
-      cargo = rust_build;
-    };
-    sample_package = naersk_lib.buildPackage {
-      pname = "example_kernel";
-      root = ./.;
-      cargoBuild = orig: "CARGO_BUILD_TARGET_DIR=$out cargo rustc  -- -Clink-arg=-nostartfiles";
-
-    };
+      naersk_lib = naersk.lib."${system}".override {
+        rustc = rust_build;
+        cargo = rust_build;
+      };
+      sample_package = naersk_lib.buildPackage {
+        pname = "example_kernel";
+        root = ./.;
+        cargoBuild = orig: "CARGO_BUILD_TARGET_DIR=$out cargo rustc --release --target=\"riscv64imac-unknown-none-elf\" -- -Clink-arg=-Tlinker.ld -Ccode-model=medium";
+      };
   in
   {
     devShell.x86_64-linux = pkgs.mkShell {
-      nativeBuildInputs = [ rust_build riscvPkgs.buildPackages.gcc ];
+      nativeBuildInputs = [ pkgs.qemu rust_build riscvPkgs.buildPackages.gcc riscvPkgs.buildPackages.gdb];
     };
     packages.riscv64-linux.kernel = sample_package;
     packages.riscv64-linux.defaultPackage = sample_package;
+    apps.x86_64-linux.toy_kernel = {
+      type = "app";
+      program = "${pkgs.qemu}/bin/qemu-system-riscv64 -bios ${sample_package}/riscv64imac-unknown-none-elf/release/nix_example_kernel -machine sifive_u";
+    };
+    defaultApp.x86_64-linux = self.apps.x86_64-linux.toy_kernel;
   };
- 
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,65 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
+#![feature(naked_functions)]
 
 use core::panic::PanicInfo;
 
+extern "C" {
+    static __global_pointer: usize;
+    static _end_stack: usize;
+}
+
+#[naked]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    unsafe {
+        asm!(
+            "
+                lui  gp, %hi({gp})
+                addi gp, gp, %lo({gp})
+                lui  sp, %hi({end_stack})
+                addi sp, sp, %lo({end_stack})
+                j main
+            ",
+            gp = const 0x80000000 as u64,
+            end_stack = const 0x80200000 as u64,
+            options(noreturn)
+        );
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    unsafe {
+        *(0x10010008 as *const u8 as *mut u8) = 1;
+        *(0x1001000C as *const u8 as *mut u8) = 1;
+        *(0x10010010 as *const u8 as *mut u8) = 0;
+        write_char('h' as u8);
+        write_char('i' as u8);
+        write_char('\n' as u8);
+    }
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+//"
+////li a7, 0x1
+////lb a0, $0
+//ecall
+//",
+
+unsafe fn write_char(ch: u8) {
+    while *(0x10010000 as *const u8 as *mut u8) <= 0 {}
+    *(0x10010000 as *const u8 as *mut u8) = ch;
+    //llvm_asm!(
+    //"
+    //li a7, 0x1
+    //lb a0, $0
+    //ecall
+    //" :: "r"(ch):
+    //);
 }
